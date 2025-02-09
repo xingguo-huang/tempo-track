ed7f9d4eea755874b26ae67ad60f3b48817d3a93
Xingguo Huang <76279571+308241561@users.noreply.github.com>
2025-02-09 14:26:29 -0800

    Initial Commit

---
+++ TempoTrack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TempoTrack
+//
+//  Created by hxg on 2/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TempoTrack/TempoTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempoTrackApp.swift
+//  TempoTrack
+//
+//  Created by hxg on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct TempoTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
